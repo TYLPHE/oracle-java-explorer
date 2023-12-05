b6ed3d658b3e067ed4db4b72fd1f482eb1b9b0c4
tylphe <tylphet@gmail.com>
2023-12-04 20:50:39 -0700

    Completed section 3.1

--- ShopApp/src/duke/choice/Main.java
+++ ShopApp/src/duke/choice/Main.java
@@ -2,6 +2,11 @@ package duke.choice;
 
 public class Main {
     public static void main(String[] args) {
+        
         System.out.println("Welcome to Duke Choice Shop");
+        
+        Customer c1 = new Customer();
+        c1.name = "Pinky";
+        System.out.println("c1.name = " + c1.name);
     }
 }
